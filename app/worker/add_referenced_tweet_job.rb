8a3a76317f42ceae82cf8ad9dd614c6c44d3d702
telzul <robert.terbach@googlemail.com>
2015-02-05 20:07:25 +0100

    fixed int/str confusion

--- app/worker/add_referenced_tweet_job.rb
+++ app/worker/add_referenced_tweet_job.rb
@@ -1,8 +1,10 @@
 class AddReferencedTweetJob
   include Sidekiq::Worker
 
+  # tweet_id describes the tweet now being created
+  # reply_id describes the tweet this one is a reply to
   def perform(tweet_id, reply_id)
-    tweet=Tweet.find_by(:twitter_id => tweet_id)
+    tweet=Tweet.find_by(:twitter_id => tweet_id.to_s)
 
     unless tweet
       twitter_tweet = $twitter_rest.status(tweet_id)
@@ -12,8 +14,9 @@ class AddReferencedTweetJob
         AddReferencedTweetJob.perform_async(twitter_tweet.in_reply_to_status_id, twitter_tweet.id)
       end
     end
-
-    tweet.replies << Tweet.find_by(:twitter_id => reply_id)
+    original_tweet = Tweet.find_by(:twitter_id => reply_id.to_s)
+    tweet.replies << original_tweet
+    tweet.topics.concat (original_tweet.topics)
     tweet.save
   end
 end
